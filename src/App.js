a7676145de2cf458ea4d7e3d1f65653ceb0a7caa
davimbastos <davimbastos@gmail.com>
2020-07-22 21:03:14 -0300

    Correção na chamada do arquivo json

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import React, { useState, useEffect} from 'react'
-import axios from 'axios'
 
 import Header from './components/ui/Header'
 import CharactersGrip from './components/characters/CharactersGrid'
 import Search from './components/ui/Search'
 import './App.css';
 
-const digimons = require('./db')
+const db = require('./db')
 
-
-const digimon = digimons.map(digimon => digimon.digimon_1)
-
-const teste = digimon.map(teste => teste.img)
-
-console.log(teste)
-console.log(digimon)
 
 const App = () => {
   const [items, setItems] = useState([])
@@ -25,23 +17,27 @@ const App = () => {
 
   useEffect(() => {
     const fetchItems = async () => {
-      const results = await axios(`https://digimon-api.herokuapp.com/api/digimon/?name=${query}`)
+      
+    const digimons = db.map(digimons => digimons.digimon_id)
 
-      setItems(results.data)
-      setIsLoading(false)
+    //console.log(digimons)
+    
+    setItems(digimons)
+    setIsLoading(false)
     }
 
     fetchItems()
   }, [query])
 
-
   return (
     <div className="container">
       <Header />
       <Search getQuery={(query) => setQuery(query)} />
-      <CharactersGrip isLoading={isLoading} items={items} />
+      <CharactersGrip isLoading={isLoading} items={items}
+      />
     </div>
   );
+
 }
 
 export default App;
